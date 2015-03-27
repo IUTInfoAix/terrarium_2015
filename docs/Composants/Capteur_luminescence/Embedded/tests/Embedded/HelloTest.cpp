7f72010b63a69436706ecdeabe6adfdbb7701350
mike1796 <mickael_brunel@hotmail.fr>
2015-03-27 17:34:28 +0100

    Ajout de test

--- docs/Composants/Capteur_luminescence/Embedded/tests/Embedded/HelloTest.cpp
+++ docs/Composants/Capteur_luminescence/Embedded/tests/Embedded/HelloTest.cpp
@@ -6,6 +6,8 @@ extern "C"
   #include "Hello.h"
 }
 
+#define DELTA 0.01
+
 TEST_GROUP(Embedded)
 {
   void setup()
@@ -17,6 +19,35 @@ TEST_GROUP(Embedded)
   }
 };
 
-TEST(Embedded, Count_1) {
-    CHECK_EQUAL(1, hello());
+/*
+**	Appliquer U=RI
+*	On cherche I donc I = U/R
+* 	R est une valeur fixe 3500  
+*	Tu vas voir dans la doc du TEMT6000
+*	Retrouver une fonction qui en fonction de l'amperage renvoie une valeur en LUX
+*
+*
+*/
+
+
+
+TEST(Embedded, CountTens1)
+{
+    DOUBLES_EQUAL(0.906, BitToTens(1035), DELTA);
 }
+
+TEST(Embedded, CountTens2)
+{
+	DOUBLES_EQUAL(2, BitToTens(2276), DELTA);
+}
+
+TEST(Embedded, CountIntens1) 
+{
+	DOUBLES_EQUAL(0.001, TensToIntens(3.5), DELTA);
+}
+
+TEST(Embedded, CountIntens2) 
+{
+	DOUBLES_EQUAL(0.0002, TensToIntens(0.7), DELTA);
+}
+
