49e91d3b1b05fbfd24d357a6b1e65ec2bb86f084
mike1796 <mickael_brunel@hotmail.fr>
2015-04-02 11:03:08 +0200

    Petite erreur rectifié sur le précedent commit

--- myapp/Groupe_A/Capteur_luminescence/Embedded/include/Embedded/CapteurLuminance.h
+++ myapp/Groupe_A/Capteur_luminescence/Embedded/include/Embedded/CapteurLuminance.h
@@ -1,5 +1,5 @@
-#ifndef EMBEDDED_HELLO_H
-#define EMBEDDED_HELLO_H
+#ifndef EMBEDDED_CAPTEURLUMINANCE_H
+#define EMBEDDED_CAPTEURLUMINANCE_H
 
 double BitToTens(int bit);
 double TensToIntens (double Tens);
