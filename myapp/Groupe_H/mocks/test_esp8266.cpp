8f18fe45c4bf2b724a10d40007b53823b9dfd761
Elekhyr <thomas.margier@live.Fr>
2015-04-03 23:40:35 +0200

    add test_esp8266

---
+++ myapp/Groupe_H/mocks/test_esp8266.cpp
@@ -0,0 +1,34 @@
+TEST_GROUP(esp8266_Tests)
+{
+};
+
+
+TEST(esp8266_Tests, wifi_connect)
+{
+	
+}
+
+TEST(esp8266_Tests, wifi_disconnect)
+{
+
+}
+
+TEST(esp8266_Tests, wifi_list)
+{
+
+}
+
+TEST(esp8266_Tests, tcp_connect)
+{
+
+}
+
+TEST(esp8266_Tests, tcp_disconnect)
+{
+
+}
+
+TEST(esp8266_Tests, tcp_status)
+{
+
+}
