3afcbbd2f7361230b1258e2e059f2281e6c01a9e
RemyKaloustian <remy.kaloustian@gmail.com>
2015-04-03 14:48:21 +0200

    Ajout de Mock_Test_Servomotor.c

---
+++ myapp/Groupe_A/Servo_moteur/Embedded/tests/Mock_Test_Servomotor.c
@@ -0,0 +1,45 @@
+/*
+*		Mock_Test_Servomotor.c
+*
+*		Fichier de test pour la partie servomoteur - Avec mock
+*
+*		Auteur : Rémy Kaloustian
+*
+*		Dernière modification : 3/04/2015
+*/
+
+
+TEST_GROUP(MockDocumentation)
+{
+    void teardown()
+    {
+        mock().clear();
+    }
+};
+
+TEST(MockDocumentation,Paramètre int valide)
+{
+
+}
+
+TEST(MockDocumentation, Paramètre int invalide)
+{
+
+}
+
+TEST(MockDocumentation, Paramètre String valide)
+{
+
+}
+
+TEST(MockDocumentation, Paramètre String invalide)
+{
+
+}
+
+TEST(MockDocumentation, ??????????)
+{
+
+}
+
+
