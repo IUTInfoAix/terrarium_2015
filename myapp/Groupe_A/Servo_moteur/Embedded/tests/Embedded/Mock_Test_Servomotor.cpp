e6a09a428f8218a31cf193e6451f8b55acf7ccf7
RemyKaloustian <remy.kaloustian@gmail.com>
2015-04-03 17:08:21 +0200

    ajout du fichier oublie de test

---
+++ myapp/Groupe_A/Servo_moteur/Embedded/tests/Embedded/Mock_Test_Servomotor.cpp
@@ -0,0 +1,52 @@
+/*
+*   Mock_Test_Servomotor.cpp
+*
+*   Fichier de test pour la partie servomoteur - Avec mock
+*
+*   Auteur : Rémy Kaloustian
+*
+*   Dernière modification : 3/04/2015
+*/
+
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+
+extern "C"
+{
+  #include <stdio.h>
+  #include <unistd.h>
+  #include "Servomotor.h"
+}
+
+#define DELTA 0.01
+TEST_GROUP(Embedded)
+{
+    void teardown()
+    {
+        mock().clear();
+    }
+};
+
+TEST(Embedded,int_parameter_valid)
+{
+  DOUBLES_EQUAL(0,Servomotor(90),DELTA);
+}
+
+TEST(Embedded, int_parameter_invalide)
+{
+  DOUBLES_EQUAL(-1,Servomotor(2000000000000),DELTA);
+}
+
+TEST(Embedded, string_parameter_valid)
+{
+  DOUBLES_EQUAL(0,Servomotor("half",DELTA);
+}
+
+TEST(Embedded, string_parameter_invalid)
+{
+  DOUBLES_EQUAL(-1,Servomotor("cuzmblakyo"),DELTA);
+}
+
+
+
