25901b57bfcede07de2f9eb534bbd8e60911fb05
ahmidou13 <ahmed.belamri13@hotmail.fr>
2015-04-07 11:44:29 +0200

    Ajout

---
+++ docs/Composants/Capteur_luminescence/Embedded/build/tests/CMakeFiles/EmbeddedTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/EmbeddedTest.dir/Embedded/HelloTest.cpp.o"
+  "../lib/libEmbeddedTest.pdb"
+  "../lib/libEmbeddedTest.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/EmbeddedTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- myapp/Groupe_A/Servo_moteur/Embedded/include/Embedded/Hello.h
+++
@@ -1,6 +0,0 @@
-#ifndef EMBEDDED_HELLO_H
-#define EMBEDDED_HELLO_H
-
-int hello();
-
-#endif
