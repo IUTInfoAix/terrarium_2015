2f2b363d085363a3ecd26e00593282f5a6210a1f
RemyKaloustian <remy.kaloustian@gmail.com>
2015-04-07 09:42:02 +0200

    Ajout de pwm.c et MAJ de Servomotor.c

--- apps/terrarium/Groupe_A_Servo_Moteur/Embedded/src/Embedded/ServoMoteur.c
+++ apps/terrarium/Groupe_A_Servo_Moteur/Embedded/src/Embedded/ServoMoteur.c
@@ -1,6 +1,28 @@
+/*
+*
+*		Fichier : ServoMoteur.c
+*
+*		Contient le comportement simulé du servomoteur
+*
+*		Dernière modification : mardi 7 avril 2015
+*
+*	==============================================================
+*
+*	-----------------------FONCTIONS A UTILISER-------------------
+
+
+static int     pwm_open(FAR struct file *filep);
+
+static ssize_t pwm_write(FAR struct file *filep, FAR const char *buffer, size_t buflen);
+
+static int     pwm_start(FAR struct pwm_upperhalf_s *upper, unsigned int oflags);
+
+static int     pwm_ioctl(FAR struct file *filep, int cmd, unsigned long arg);
+
+NB: Voir pwm.c dans le même dossier
+
+*/
+
 #include "ServoMoteur.h"
 
-int hello () 
-{
-	return 1;
-}
+void initializeServomoteur()
