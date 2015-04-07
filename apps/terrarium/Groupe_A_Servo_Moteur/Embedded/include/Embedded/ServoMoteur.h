478c6951906a75259249210eff2669bce8d056b2
Raphael KUMAR <etudes.kumar@orange.fr>
2015-04-07 10:59:47 +0200

    Servo.h

--- apps/terrarium/Groupe_A_Servo_Moteur/Embedded/include/Embedded/ServoMoteur.h
+++ apps/terrarium/Groupe_A_Servo_Moteur/Embedded/include/Embedded/ServoMoteur.h
@@ -1,6 +1,11 @@
 #ifndef EMBEDDED_HELLO_H
 #define EMBEDDED_HELLO_H
 
-int hello();
+
+void initializeServomoteur(int (*ServoMoteur)(int positionAngulaire));
+int StrPostoString(String posText);
+
+int mouvement(String val);
+int mouvement(int val);
 
 #endif
