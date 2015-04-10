9f4e8d31305eec10759040129c67ae063cc2fe1f
RemyKaloustian <remy.kaloustian@gmail.com>
2015-04-10 17:04:47 +0200

    bigPwm et autres sur servomoteur

--- apps/terrarium/Groupe_A_Servo_Moteur/Embedded/include/Embedded/ServoMoteur.h
+++ apps/terrarium/Groupe_A_Servo_Moteur/Embedded/include/Embedded/ServoMoteur.h
@@ -1,10 +1,13 @@
 #ifndef EMBEDDED_HELLO_H
 #define EMBEDDED_HELLO_H
 
+#include <stdio.h>
+
 
 void initializeServomoteur(int (*Mouvement)(int positionAngulaire));
-void initializeServomoteurSplit(int (*BoardInitialize)(int frontMontant, int pulseTime, int countPulse ), int (*start()));
-
+void initializeServomoteurSplit(int (*BoardInitialize)(int frontMontant, int pulseTime, int duration ), 
+	int (*start(int devpath)));
+ 
 int StrPostoInt(char posText[]);
 
 int ServoMoteurStr(char val[]);
