eb4251b4bbc1c6f1b75053748990dc00e9b22e63
Alexis EUTROPE <alexis.eutrope@gmail.com>
2015-04-19 15:36:57 +0200

    update mocks

--- apps/terrarium/Groupe_H/Embedded/include/Embedded/mock_esp8266.cpp
+++ apps/terrarium/Groupe_H/Embedded/include/Embedded/mock_esp8266.cpp
@@ -1,14 +1,42 @@
+#include <sys/types.h>
+#include "mock_esp8266.h"
+#include "string.h"
+#include "myErrno.h"
 
-int write(){
+int mock_esp8266::mock_open(const char *path, int oflag){
+	if (strcmp("/dev/ttyS0", path) && (oflag == 0)){
+		mock_esp8266::fd = 32987;
+		return mock_esp8266::fd;
+	}else{
+		mock_esp8266::fd = -1;
+		return -1;
+	}
+}
+
+int mock_esp8266::mock_close(int fd){
+	if (mock_esp8266::fd == fd && (fd > 0)){
+		mock_esp8266::fd = -1;
+		return 0;
+	}
+	else
+		return -1;
+}
+
+ssize_t mock_esp8266::mock_read(int fd, void *buf, size_t nbytes){
+	if(mock_esp8266::fd == fd){
+		return  nbytes;
+	}
+	return 0;
+	
 
 }
 
-int close(){
-
+ssize_t mock_esp8266::mock_write(int fd, const void *buf, size_t nbytes){
+	return 0;
 }
+
 
 int main(){
 	return 0;
 }
-
 
---
+++ apps/terrarium/Groupe_H/Embedded/include/Embedded/mock_esp8266.h
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+
+class mock_esp8266 {
+	static int fd;
+public:
+	int 	mock_open(const char *path, int oflag);
+	int     mock_close(int fd);
+	ssize_t mock_read(int fd, void *buf, size_t nbytes);
+	ssize_t mock_write(int fd, const void *buf, size_t nbytes);
+};
